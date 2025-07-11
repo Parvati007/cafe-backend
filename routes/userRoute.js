f3b842e779ae433dd801a3d87b936f8c06ecf849
Parvati007 <parvati000777@gmail.com>
2025-07-11 21:09:39 +0530

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -23,8 +23,9 @@ Router.get("/showusers",showusers)
 //updating the user as admin API
 Router.patch("/:id",authenticate,authorize("admin"),updateUser)
 
-//deleting the user as admin API
-Router.delete("/:id",authenticate,authorize("admin"),deleteUser)
+// //deleting the user as admin API
+// Router.delete("/:id",authenticate,authorize("admin"),deleteUser)
+Router.delete("/:id", deleteUser);
 
 //showing only one user details as admin API
 Router.get("/:id/profile",authenticate,profileUser)
