5078411b14535250edf0001cc2d0cf848e19a718
Parvati007 <parvati000777@gmail.com>
2025-07-19 23:01:12 +0530

    updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -111,7 +111,7 @@ const addUser = async (req, res) => {
     res.status(200).json(result);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Something went wrong" });
-  }
+    res.status(500).json({ message: "Something went wrong" });
+  }
 }
  export {register,login,showusers,updateUser,deleteUser,profileUser,addUser};
